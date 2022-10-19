10fcdde4fa6200e97373bc9e8bdf8fe285764a83
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-10-19 14:59:54 +0200

    minor tweeks

--- src/ts_language_collection/langs.py
+++ src/ts_language_collection/langs.py
@@ -10,14 +10,14 @@ with open(common.lang_names_index_file(HERE), "rb") as infile:
     lang_names = pickle.load(infile)
 
 
-def get_language(language):
+def get_language(language:str) -> Language:
     assert language in lang_names, "Not a valid language option!"
     return Language(str(common.lib_filename(HERE)), language)
 
 
-def get_parser(language):
-    language = get_language(language)
+def get_parser(language:str) -> Parser:
+    lang_obj = get_language(language)
     parser = Parser()
-    parser.set_language(language)
+    parser.set_language(lang_obj)
     return parser
 
